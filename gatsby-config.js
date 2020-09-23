6cc4930649991e8d50090ec91f18a2c9ac1b29a2
vegardloewe <vegardloewe96@gmail.com>
2020-09-23 11:17:59 +0200

    commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
     title: `Clean Air Units`,
     description: `An innovative product that creates a protective zone without any particles ...`,
     author: `Clean Air Units`,
-    image: `https://ibb.co/d5NH0sw`,
+    image: `/hero.png`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
